ae57491c9551ba15b2eb4218b70a98ac143ac216
Alan Franco <sonic600@live.com.mx>
2023-11-13 18:45:32 -0600

    First commit

--- src/main/java/com/example/whatsup_certifier/CertifierController.java
+++ src/main/java/com/example/whatsup_certifier/CertifierController.java
@@ -2,9 +2,14 @@ package com.example.whatsup_certifier;
 
 import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class CertifierController implements Initializable {
 
-    SQL database = new SQL();
+    @Override public void initialize(URL location, ResourceBundle resources){
+        SQL database = new SQL();
         database.getQuery("");
+    }
 
 }
